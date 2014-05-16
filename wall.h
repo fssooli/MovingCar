b8b51313fd5c21756e7ba6f1fb68c03ccf8c9c01
kyle kersey <kylelk@comcast.net>
2014-05-16 16:49:52 -0700

    fixed texture

--- wall.h
+++ wall.h
@@ -28,24 +28,27 @@ GLuint LoadTexture(const char * bitmap_file)
 
 
 void wall() {
-    glMatrixMode(GL_MODELVIEW);
     glEnable(GL_TEXTURE_2D);
     GLuint tex1 = LoadTexture("brick.bmp");
     glPushMatrix();
         glTranslatef(0.0f, -0.2f, -1.5f);
         glBindTexture(GL_TEXTURE_2D, tex1);
         glBegin(GL_POLYGON);                // start drawing a polygon
-//            glVertex3f(5.0f, 0.15f, 0.0f);        // Top left
-//            glVertex3f(5.0f, 0.5f, 0.0f);
-//            glVertex3f(-5.0f, 0.5f, 0.0f);
-//            glVertex3f(-5.0f, 0.15f, 0.0f);
+            glTexCoord2f(5.0f, 0.15f);
+            glVertex3f(5.0f, 0.15f, 0.0f);        // Top left
+    
+            glTexCoord2f(5.0f, 0.5f);
+            glVertex3f(5.0f, 0.5f, 0.0f);
+    
+            glTexCoord2f(-5.0f, 0.5f);
+            glVertex3f(-5.0f, 0.5f, 0.0f);
+    
+            glTexCoord2f(-5.0f, 0.15f);
+            glVertex3f(-5.0f, 0.15f, 0.0f);
 //            //glVertex3f(-0.2f,0.1f, 0.0f);        // Bottom Right
 //            //glVertex3f(-0.2f,-0.15f, 0.0f);		   // Bottom Left
-    glTexCoord2f(0.0f, 0.0f); glVertex3f(-0.5f, -0.5f, 0.0f);
-    glTexCoord2f(1.0f, 0.0f); glVertex3f( 0.5f, -0.5f, 0.0f);
-    glTexCoord2f(1.0f, 1.0f); glVertex3f( 0.5f,  0.5f, 0.0f);
-    glTexCoord2f(0.0f, 1.0f); glVertex3f(-0.5f,  0.5f, 0.0f);
         glEnd();
 	glPopMatrix();
     
+    glDisable(GL_TEXTURE_2D);
 }
